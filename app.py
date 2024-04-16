a7f177c4d451b61b453ff129ab3e5a6e3e1f1268
ArshadTechie <154262231+ArshadTechie@users.noreply.github.com>
2024-04-16 12:36:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 from streamlit import *
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
