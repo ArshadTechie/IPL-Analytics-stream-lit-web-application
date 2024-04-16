f6065aba9f67967be4e5af5efc56d3215c154bc2
ArshadTechie <154262231+ArshadTechie@users.noreply.github.com>
2024-04-16 12:53:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 from streamlit import *
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 #Title of the project
 st.title("IPL Analytics")
